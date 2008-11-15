7707807d886d3d42f841597aff25d726fcd0d663
Shoaib Burq <saburq@gmail.com>
2008-11-15 21:22:10 +1100

    stub spec for provider

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ end
 require "merb-core"
 require "spec" # Satisfies Autotest and anyone else not using the Rake tasks
 require(File.dirname(__FILE__) / "spec_helpers.rb")
+require File.expand_path(File.dirname(__FILE__) + "/blueprints")
 
 # this loads all plugins required in your init file so don't add them
 # here again, Merb will do it for you
@@ -19,4 +20,5 @@ Spec::Runner.configure do |config|
   config.include(Merb::Test::RouteHelper)
   config.include(Merb::Test::ControllerHelper)
   config.include(GeoPing::SpecHelpers)
+  config.before(:each) { Sham.reset }  
 end
