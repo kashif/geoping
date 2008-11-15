c989df34489fecd81f980b40dc8dc14b0094ccf1
Shoaib Burq <saburq@gmail.com>
2008-11-16 00:46:43 +1100

    speced the provider model

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,6 +1,6 @@
 class Provider < ActiveRecord::Base
-  # validates_presence_of   :identity_url
-  validates_uniqueness_of  :identity_url, :allow_blank => true
+  validates_presence_of     :identity_url
+  validates_uniqueness_of   :identity_url, :allow_blank => true
   
   has_many :sites
   has_many :pings, :through => :sites
