f743b00acc32460257ab3ce02ed0cd19aded4e6e
kashif <kashif@nomad-labs.com>
2009-12-25 01:27:49 +0100

    Updated to latest versions of merb etc.

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,6 +1,6 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.6.1"
-dm_gems_version   = "0.9.7"
+merb_gems_version = "1.0.15"
+dm_gems_version   = "~> 0.10"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-helpers", merb_gems_version
@@ -11,15 +11,15 @@ dependency "merb-slices", merb_gems_version
 dependency "merb-param-protection", merb_gems_version
 dependency "merb-exceptions", merb_gems_version
 
-dependency "nokogiri", ">=1.0.2"
+dependency "nokogiri",          ">=1.4.1"
 
-dependency "postgres",          ">= 0.7.9.2008.01.28"
-dependency "activerecord",      "= 2.1.2", :immediate => true
-dependency "merb_activerecord", "0.9.9"
+dependency "pg",                ">= 0.8.0"
+dependency "activerecord",      "= 2.3.5", :immediate => true
+dependency "merb_activerecord", "1.0.0.1"
 
 # need to give activerecord a chance to load before hitting this one
 Merb::BootLoader.after_app_loads do
-  dependency "GeoRuby",           "= 1.3.3", :require_as => "geo_ruby"
-  dependency "merb_has_rails_plugins", ">= 0.1.0"
+  dependency "GeoRuby",                 "= 1.6.3", :require_as => "geo_ruby"
+  dependency "merb_has_rails_plugins",  ">= 0.1.0"
 end
 
